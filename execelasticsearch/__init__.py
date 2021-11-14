46a8a337ac78156ee3d569df8afe9d28d81ffa26
supplayer <x254724521@hotmail.com>
2021-11-14 10:35:30 +0800

    add new class ClientConfig

--- execelasticsearch/__init__.py
+++ execelasticsearch/__init__.py
@@ -1,8 +1,8 @@
-from execelasticsearch.connection import Clients
+from execelasticsearch.connection import ClientConfig
 from execelasticsearch.handler import ExecES
 
 
 __all__ = [
-    'Clients',
+    'ClientConfig',
     'ExecES'
 ]
