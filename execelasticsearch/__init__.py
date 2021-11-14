0602febb1fc7f6b176330f26cf4041d94b7f8eb4
supplayer <x254724521@hotmail.com>
2021-11-14 16:17:15 +0800

    add mapping.py

--- execelasticsearch/__init__.py
+++ execelasticsearch/__init__.py
@@ -1,8 +1,10 @@
+from execelasticsearch.mapping import MappingData
 from execelasticsearch.connection import ClientConfig
 from execelasticsearch.handler import ExecES
 
 
 __all__ = [
+    'MappingData',
     'ClientConfig',
     'ExecES'
 ]
